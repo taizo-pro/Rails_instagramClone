6815ddc90543df66754c05bd6162b0e7600de6fb
taizo-pro <t2s.single@gmail.com>
2020-10-07 13:59:47 +0900

    development change commit

---
+++ app/controllers/accounts_controller.rb
@@ -0,0 +1,11 @@
+class AccountsController < ApplicationController
+
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :accounts
+  # dashboard
+  get "/dashboard" => "accounts#index"
+
+  root to: "public#homepage"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
